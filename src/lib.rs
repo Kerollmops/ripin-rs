7eb9edb83b4c83e2ce9ebaa5973f339632d14ead
Kerollmops <renault.cle@gmail.com>
2016-12-31 00:26:28 +0100

    Revert "stack: Remove Stack from public layer"
    This reverts commit 328d343650bca6c9808bc73bed870001cc2967f9.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ pub mod stack;
 pub mod expression;
 pub mod operate;
 
-pub fn pop_two_operands<T>(stack: &mut stack::Stack<T>) -> Option<(T, T)> {
+pub use stack::Stack;
+
+pub fn pop_two_operands<T>(stack: &mut Stack<T>) -> Option<(T, T)> {
     match (stack.pop(), stack.pop()) {
         (Some(a), Some(b)) => Some((b, a)),
         _ => None,
